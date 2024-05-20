470538647102eb2741c6909623c37d304e6545e6
Floururine.com <votruong830430073@gmail.com>
2024-05-20 13:37:58 +0700

    FristTest

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VHECIntershipMain.Models;
+
+namespace VHECIntershipMain.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = new List<UserModel> {
+                new UserModel { 
+                    Id = 1,
+                    Name = "TruongVM", 
+                    Email = "truong@gmail.com",
+                    Password = "123456789"}
+            };
+            return Ok(users);
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace VHECIntershipMain.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }   
+        public required string Password { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public DateTime UpdateDate { get; set; } = DateTime.Now;
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+    }
+}
